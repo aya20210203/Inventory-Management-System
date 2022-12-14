9eedb07bd882d5723770e0f0cd66cd316e0f578a
AyaAshrafMohammed <totatota221223@gmail.com>
2022-12-14 17:34:24 +0200

    gui

---
+++ Aya_20210204/Inventory-Mangment-System/src/main/java/com/mycompany/inventory/mangment/system/Inventory.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.inventory.mangment.system;
+
+/**
+ *
+ * @author asus
+ */
+public class Inventory {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+    }
+    
+}
